945bc12fb17a8e6dba535d76197e026a6442006e
nqazimn <nqazimn@gmail.com>
2019-12-10 13:54:44 -0600

    Added code to read csv data from file. Defined function to display list on terminal for debugging purposes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,26 @@
 import os
 import csv
 
+def display_list(a_list):
+    # Print items of a list on the terminal
+    for item in a_list:
+        print(item)
+
 path_to_file = os.path.join('Resources','budget_data.csv')
 
-#with open(path_to_file,'r',newline='') as csvreader:
+budget_dates=[]
+budget_profit_losses=[]
+
+with open(path_to_file, newline='',encoding="utf8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    # * Step over the headers
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        budget_dates.append(row[0])
+        budget_profit_losses.append(row[1])
+
+#display_list(budget_dates)
+#display_list(budget_profit_losses)
+
