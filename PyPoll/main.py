c91180c61b556c07d4ab4dbbce4bc053dbfc20d6
nqazimn <nqazimn@gmail.com>
2019-12-11 09:05:12 -0600

    Coded reading of csv file and storing data for analysis

---
+++ PyPoll/main.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+
+def display_list(a_list):
+    # Print items of a list on the terminal
+    for item in a_list:
+        print(item)
+
+
+path_to_file = os.path.join('Resources', 'election_data.csv')
+
+voter_IDs = []
+counties = []
+candidates = []
+
+with open(path_to_file, newline='', encoding="utf8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # * Step over the headers
+    csv_header = next(csvreader)
+
+ # Read and store data
+    for row in csvreader:
+        voter_IDs.append(int(row[0]))
+        counties.append(row[1])
+        candidates.append(row[2])
+
+# print(len(voter_IDs))
